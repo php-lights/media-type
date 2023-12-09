c9e92fe0c2e56b95be5879595c85d7f4c8735b0f
Samantha Nguyen <contact@samanthanguyen.me>
2023-12-08 18:22:08 -0600

    tests: add more test cases for Utf8Utils

--- tests/Utf8UtilsTest.php
+++ tests/Utf8UtilsTest.php
@@ -2,6 +2,7 @@
 
 namespace Neoncitylights\MediaType\Tests;
 
+use IntlChar;
 use Neoncitylights\MediaType\Utf8Utils;
 use PHPUnit\Framework\TestCase;
 
@@ -9,6 +10,21 @@ use PHPUnit\Framework\TestCase;
  * @coversDefaultClass \Neoncitylights\MediaType\Utf8Utils
  */
 class Utf8UtilsTest extends TestCase {
+	/**
+	 * @covers ::onlyContains
+	 * @dataProvider provideOnlyContains
+	 */
+	public function testOnlyContains(
+		string $input,
+		callable $predicateFn,
+		bool $expected
+	): void {
+		$this->assertEquals(
+			$expected,
+			Utf8Utils::onlyContains( $input, $predicateFn )
+		);
+	}
+
 	/**
 	 * @covers ::trimHttpWhitespace
 	 * @dataProvider provideTrimHttpWhitespace
@@ -37,10 +53,38 @@ class Utf8UtilsTest extends TestCase {
 		$position = $originalPosition;
 
 		$this->assertEquals(
-			Utf8Utils::collectCodepoints( $originalString, $position, $predicateFn ),
-			$newString
+			$newString,
+			Utf8Utils::collectCodepoints( $originalString, $position, $predicateFn )
 		);
 		$this->assertEquals( $position, $newPosition );
+	}
+
+	/**
+	 * @covers ::isHttpTokenCodepoint
+	 * @dataProvider provideIsHttpTokenCodepoint
+	 */
+	public function testIsHttpTokenCodepoint(
+		string $codepoint,
+		bool $expected
+	): void {
+		$this->assertEquals(
+			$expected,
+			Utf8Utils::isHttpTokenCodepoint( $codepoint )
+		);
+	}
+
+	/**
+	 * @covers ::isHttpQuotedStringTokenCodepoint
+	 * @dataProvider provideIsHttpQuotedStringTokenCodepoint
+	 */
+	public function testIsHttpQuotedStringTokenCodepoint(
+		string $codepoint,
+		bool $expected
+	): void {
+		$this->assertEquals(
+			$expected,
+			Utf8Utils::isHttpQuotedStringTokenCodepoint( $codepoint )
+		);
 	}
 
 	/**
@@ -51,23 +95,55 @@ class Utf8UtilsTest extends TestCase {
 		string $input,
 		bool $expected
 	): void {
-		$this->assertEquals( $expected, Utf8Utils::isHttpWhitespace( $input ) );
+		$this->assertEquals(
+			$expected,
+			Utf8Utils::isHttpWhitespace( $input )
+		);
+	}
+
+	public function provideOnlyContains(): array {
+		return [
+			[ '', fn( string $c ) => \ctype_alpha( $c ), true ],
+			[ 'test', fn( string $c ) => \ctype_alpha( $c ), true ],
+			[ '1234', fn( string $c ) => \ctype_digit( $c ), true ],
+			[ '1234test', fn( string $c ) => \ctype_digit( $c ), false ],
+			[ 'test1234', fn( string $c ) => \ctype_alpha( $c ), false ],
+			[ 'test1234', fn( string $c ) => \ctype_alnum( $c ), true ],
+		];
 	}
 
 	public function provideTrimHttpWhitespace(): array {
 		return [
 			[ '', '' ],
 			[ ' test ', 'test' ],
+			[ '      ', '' ],
 		];
 	}
 
 	public function provideCollectCodepoints(): array {
 		return [
 			[ '', 0, '', 0, fn() => true ],
-			[ '1234test', 0, '1234', 4, fn(string $s) => \ctype_digit($s)],
-			[ 'test1234', 0, 'test', 4, fn(string $s) => \ctype_alpha($s)],
-			[ 'foo/bar', 0, 'foo', 3, fn(string $s) => $s !== '/' ],
-			[ 'foo/bar;', 0, 'foo/bar', 7, fn(string $s) => $s !== ';' ],
+			[ '1234test', 0, '1234', 4, fn( string $s ) => \ctype_digit( $s ) ],
+			[ 'test1234', 0, 'test', 4, fn( string $s ) => \ctype_alpha( $s ) ],
+			[ 'foo/bar', 0, 'foo', 3, fn( string $s ) => $s !== '/' ],
+			[ 'foo/bar;', 0, 'foo/bar', 7, fn( string $s ) => $s !== ';' ],
+		];
+	}
+
+	public function provideIsHttpTokenCodepoint(): array {
+		return [
+			[ IntlChar::chr( 0x24 ), true ],
+			[ IntlChar::chr( 0x28 ), false ],
+		];
+	}
+
+	public function provideIsHttpQuotedStringTokenCodepoint(): array {
+		return [
+			[ IntlChar::chr( 0x20 ), true ],
+			[ IntlChar::chr( 0x21 ), true ],
+			[ IntlChar::chr( 0x22 ), false ],
+			[ IntlChar::chr( 0x5C ), false ],
+			[ IntlChar::chr( 0x7F ), false ],
 		];
 	}
 
