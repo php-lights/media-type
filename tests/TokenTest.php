81417b8ef5528287c7835a10a70f4bc1c5196430
Samantha Nguyen <contact@samanthanguyen.me>
2023-12-08 18:27:46 -0600

    tests: add test case for Token

---
+++ tests/TokenTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Neoncitylights\MediaType\Tests;
+
+use Neoncitylights\MediaType\Token;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversDefaultClass \Neoncitylights\MediaType\Token
+ */
+class TokenTest extends TestCase {
+    /**
+     * @coversNothing
+     */
+    public function testEnumMemberValues(): void {
+        $this->assertEquals( ';', Token::Semicolon->value );
+        $this->assertEquals( '=', Token::Equal->value );
+        $this->assertEquals( '/', Token::Slash->value );
+    }
+}
